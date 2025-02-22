98e351490abf8687ec304cba4c204c510d87a582
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2025-02-22 16:50:33 -0600

    Remove unused matplotlib import from random_forest_example.py

--- Piyush/random_forest_example.py
+++ Piyush/random_forest_example.py
@@ -1,7 +1,6 @@
 # Import necessary libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
